f3faa65eff22e8911446ddd437703d679eea0405
Gleb Kartashov <47090261+ferscalone@users.noreply.github.com>
2022-05-10 13:15:05 +0300

    #100

--- classes/algebra/arithmetic__/gcd_and_lmn.py
+++ classes/algebra/arithmetic__/gcd_and_lmn.py
@@ -23,6 +23,14 @@ class GCD_and_LMN(QWidget):
         self.b_argument.setGeometry(QRect(75, 190, 50, 50))
         self.b_argument.setValidator(QIntValidator(1, 1000000000, self))
 
+        """self.n = QLineEdit(self)
+        self.n.setGeometry(QRect(75, 350, 50, 50))
+        self.n.setValidator(QIntValidator(1, 20, self))
+
+        n_button = QPushButton("Генерация окон", self)
+        n_button.setGeometry(QRect(125, 350, 280, 100))
+        n_button.clicked.connect(self.nn)"""
+
         gcd_button = QPushButton("НОД", self)
         gcd_button.setGeometry(QRect(250, 130, 280, 100))
         gcd_button.clicked.connect(self.gcd)
@@ -37,11 +45,23 @@ class GCD_and_LMN(QWidget):
         self.svg_lmn = QSvgWidget(self)
         self.svg_lmn.setGeometry(QRect(530, 250, 100, 50))
 
+    """нод и нок для нескольких чисел, считывание с окошек итд"""
+
+    """def nn(self):
+        for i in range(1, int(self.n.text()) + 1):
+            s = 's' + str(i)
+            self.s = QLineEdit(self)
+            self.s.setGeometry(QRect(75 + (i-1)*50, 450, 50, 50))
+            self.s.setValidator(QIntValidator(1, 10, self))
+            self.s.show()
+            """
+
     def gcd(self):
         if self.a_argument.text() != '' and self.b_argument.text() != '':
             a = int(self.a_argument.text())
             b = int(self.b_argument.text())
             self.svg_gcd.load(tex2svg(latex(sympy.gcd(a, b))))
+
     def lmn(self):
         if self.a_argument.text() != '' and self.b_argument.text() != '':
             a = int(self.a_argument.text())
