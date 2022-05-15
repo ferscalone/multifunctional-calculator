77c0a5ac5d70b0eddb515b6b7b75e606e27bd317
Gleb Kartashov <47090261+ferscalone@users.noreply.github.com>
2022-05-15 20:14:01 +0300

    арифметика есть

--- classes/algebra/arithmetic__/exponentiation.py
+++ classes/algebra/arithmetic__/exponentiation.py
@@ -15,6 +15,31 @@ class Exponentiation(QWidget):
         back_button.setGeometry(QRect(250, 20, 280, 100))
         back_button.clicked.connect(self.open_parent_window)
 
+        self.a = QLineEdit(self)
+        self.a.setGeometry(QRect(25, 190, 50, 50))
+        self.a.setValidator(QDoubleValidator(0.0, 1000000000.0, 6))
+
+        self.b = QLineEdit(self)
+        self.b.setGeometry(QRect(75, 190, 50, 50))
+        self.b.setValidator(QDoubleValidator(0.0, 1000000000.0, 6))
+
+        root_button = QPushButton("Показать степень (сначала возводимое число, потом степень)", self)
+        root_button.setGeometry(QRect(250, 130, 280, 100))
+        root_button.clicked.connect(self.root)
+
+        self.svg_root = QSvgWidget(self)
+        self.svg_root.setGeometry(QRect(530, 150, 100, 20))
+
+    def root(self):
+        if self.a.text() != '' and self.b.text() != '':
+            a = self.a.text()
+            b = self.b.text()
+            a = a.replace(",", ".")
+            b = b.replace(",", ".")
+            a = float(a)
+            b = float(b)
+            self.svg_root.load(tex2svg(latex(a**b)))
+
     def open_parent_window(self):
         self.parent_window.show()
         self.close()
--- classes/algebra/arithmetic__/sum_m_n.py
+++ classes/algebra/arithmetic__/sum_m_n.py
@@ -1,3 +1,5 @@
+import sympy
+
 from modules import *
 
 
@@ -14,6 +16,30 @@ class Sum_M_N(QWidget):
         back_button = QPushButton("Назад", self)
         back_button.setGeometry(QRect(250, 20, 280, 100))
         back_button.clicked.connect(self.open_parent_window)
+
+        self.m = QLineEdit(self)
+        self.m.setGeometry(QRect(25, 190, 50, 50))
+        self.m.setValidator(QIntValidator(1, 1000000000))
+
+        self.n = QLineEdit(self)
+        self.n.setGeometry(QRect(75, 190, 50, 50))
+        self.n.setValidator(QIntValidator(1, 1000000000))
+
+        root_button = QPushButton("Сумма чисел от m до n", self)
+        root_button.setGeometry(QRect(250, 130, 280, 100))
+        root_button.clicked.connect(self.root)
+
+        self.svg_root = QSvgWidget(self)
+        self.svg_root.setGeometry(QRect(530, 150, 100, 20))
+
+    def root(self):
+        if self.m.text() != '' and self.n.text() != '':
+            m = int(self.m.text())
+            n = int(self.n.text())
+            if m <= n:
+                self.svg_root.load(tex2svg(latex(int((m+n)*(n-m+1)/2))))
+            else:
+                self.svg_root.load(tex2svg(latex(int((m + n) * (m - n + 1) / 2))))
 
     def open_parent_window(self):
         self.parent_window.show()
